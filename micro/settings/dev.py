6acfd236ccc5fb7620837b1ba8350a872a781291
USR-ON-02 <micromodern.bp@gmail.com>
2024-08-18 17:02:21 +0330

    first commit

---
+++ micro/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure--7(4a6i-nes2q$&@d=8sq108i$r@2=r43n$c744@t5ott+x67x"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
